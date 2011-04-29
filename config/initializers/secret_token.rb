b7b59019dc876a5fc057561dd70acbac2f64727b
lordedster <thegreatstreuli@gmail.com>
2011-04-29 11:31:01 -0300

    primer commit, junto con la estructura del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dovedeath::Application.config.secret_token = '3bf8fdf0db263daf505f90ab9e82f2789b98eabbd88f95179458222d46a5ca7172c798daf620ea7ddd150126bee897927f44b0831062479972294bd32879a8b3'
