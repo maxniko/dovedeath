ef92ea544129b09468f78a702ad8384f6e910c81
lordedster <thegreatstreuli@gmail.com>
2011-05-16 17:36:36 -0300

    orden de los posts por orden de publicación mas reciente

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.find(:all).paginate :page => params[:page], :order => 'created_at DESC'
+    @posts = Post.find(:all,:order => 'created_at DESC').paginate :page => params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
