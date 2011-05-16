6839f6daf6528ee9dc656d48659d865485bb7d98
lordedster <thegreatstreuli@gmail.com>
2011-05-16 19:01:22 -0300

    se guardan los nombres

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,7 +5,7 @@ class Usuario < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nombre, :apellido
   has_many :posts
 end
 
