2f0bb8043b0d91938d5ae6a8a9895e90cb88f3c9
Max Nikolaus <maxniko@gmail.com>
2011-05-18 14:12:13 -0300

    eliminé los views de Comentarios, inserté la opción de comentarios en el show de Posts. Cambios varios al routes.rb

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,2 +1,5 @@
 class Comentario < ActiveRecord::Base
+  belongs_to :post
+  belongs_to :usuario
+  validates_presence_of :cuerpo
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ActiveRecord::Base
     belongs_to :usuario
     validates_presence_of :categorium
     validates_presence_of :titulo
+    validates_presence_of :cuerpo
+    has_many :comentarios
 end
 
